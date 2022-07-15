45685a410cf02ba31a00f6315cb29a9450fea07a
cheon4050 <ehdcjs159@gmail.com>
2022-07-16 01:33:32 +0900

    feat: db연결

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-from kospeech.infer_ import pred_sentence
+# from kospeech.infer_ import pred_sentence
 
-@app.route('/model/dialectAnalysis', methods = ["POST"])
+from flask import Flask, request
+from sqlalchemy import create_engine, text
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+database = create_engine(app.config['DB_URL'], encoding = 'utf-8')
+app.database = database
+
+# 영상 데이터 받아서 사투리 관련 데이터 DB에 저장시키는 API
+@app.route('/model/video', methods = ["GET"])
 def dialectAnalysis():
+    Jwt = request.headers['Jwt']
 
-    return "json([{},{}])"
+    print(request.headers['Jwt'])
+    return request.get_json()
 
 @app.route('/model/dialectData', methods = ["GET"])
 def new_hello():
     return 'new_hello'
-from flask import Flask
-
-s = pred_sentence('a','b','c')
-print(s)
-# app = Flask(__name__)
-#
-#
-#
-# @app.route('/')
-# def hello():
-#     return 'Hello, My First Flask!'
-#
-# @app.route('/hello')
-# def new_hello():
-#     return 'new_hello'
