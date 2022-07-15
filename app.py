91abbbed3ae3769d70cb8a070dd496a83c97fb3b
cheon4050 <ehdcjs159@gmail.com>
2022-07-16 01:45:58 +0900

    feat: api 구조 설계

--- app.py
+++ app.py
@@ -9,13 +9,25 @@ database = create_engine(app.config['DB_URL'], encoding = 'utf-8')
 app.database = database
 
 # 영상 데이터 받아서 사투리 관련 데이터 DB에 저장시키는 API
-@app.route('/model/video', methods = ["GET"])
+@app.route('/model/video', methods = ["POST"])
 def dialectAnalysis():
     Jwt = request.headers['Jwt']
 
     print(request.headers['Jwt'])
     return request.get_json()
 
-@app.route('/model/dialectData', methods = ["GET"])
-def new_hello():
-    return 'new_hello'
+@app.route('/model/data/score', methods = ["GET"])
+def getDataScore():
+    return ''
+
+@app.route('/model/score/average', methods= ["GET"])
+def getDataScoreAverage():
+    return ''
+
+@app.route('/model/data/list', methods= ["GET"])
+def getDataList():
+    return ''
+
+@app.route('/model/data/detail', methods= ["GET"])
+def getDataDetail():
+    return ''
