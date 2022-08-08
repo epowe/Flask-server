5198441950c3006ff6ddb8c8d2e08b937fafdf8c
cheon4050 <ehdcjs159@gmail.com>
2022-08-08 13:17:09 +0900

    fix: accessToken Header구조 변경

--- jwtUtil.py
+++ jwtUtil.py
@@ -1,12 +1,14 @@
 import time
 import jwt
 
-def valid(accessToken):
+def valid(Authorization):
     try:
+        type, accessToken = Authorization.split()
         temp = jwt.decode(accessToken, "이에이승팀의샘플비밀키입니다.", algorithms="HS256")
         userIdx = temp["userIdx"]
         exp = temp["exp"]
-        if exp < int(time.time()):
+
+        if exp < int(time.time()) or type != "Bearer":
             return 401, 0
         else:
             return 200, userIdx
